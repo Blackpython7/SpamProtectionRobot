73dee08a483f11a880a6f779762457e729b3cb33
AVYA <136340837+Blackpython7@users.noreply.github.com>
2025-06-27 16:40:42 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -51,14 +51,14 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
+            "Repo": "https://t.me/+eqE0gJDY7KE0ZTA1",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+            "Support Chat (for now)": "https://t.me/+rI_8VXa0gzplZmU1",
         }
     )
     mention = message.from_user.mention
     await message.reply_photo(
-        "https://hamker.me/logo_3.png",
+        "https://ibb.co/ynsWqP9g",
         caption=f"Hi {mention}, I'm SpamProtectionRobot,"
         + " Choose An Option From Below.",
         reply_markup=kb,
